797240a853db600a3bf5eca6931808532118c39e
Hassan Kuntze <hskuntze@gmail.com>
2020-05-05 16:31:25 -0300

    Seller Entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Department dp = new Department("Nas", 2);
+		Seller sl = new Seller(21, "bob", "bob@gmail.com", new Date(), 2300.00, dp);
 		
+		System.out.println(sl);
 	}
 }
